0a2456e57acb7a22400b01e45f0aab58c44d760e
Marcelo Claro <marc01mcl@hotmail.com>
2023-05-04 10:21:30 -0300

    components createds with styles

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+})
+export class MenuTitleComponent {}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+})
+export class SmallCardComponent {}
